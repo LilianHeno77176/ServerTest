cb1ab3cdbea0005530ebcd48df375b0edbf9af01
idyol <ihssandk@gmail.com>
2022-11-18 18:54:45 +0100

    server/index.js modified to deploy on render

--- server/index.js
+++ server/index.js
@@ -4,9 +4,19 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.get('/api', (req,res)=>{
-    res.json({message : "Hello from the server!"});
-});
 app.listen( PORT, ()=>{
     console.log(`Server listening on ${PORT}`);
+});
+
+// Have Node serve the files for our build React App
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+//Handle GET requests to /api route
+app.get('/api', (req,res)=>{
+    res.json({message : 'Hello from the server!'})
+});
+
+// All oher GET requests not handled before will return our React app
+app.get("*", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
