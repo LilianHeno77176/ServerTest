252d2082f70273424b13749dfba2f83510bc104d
idyol <ihssandk@gmail.com>
2022-11-20 15:27:19 +0100

    package.json + build

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+    res.json({ message: "Hello from server y'all!" });
   });
 
 
